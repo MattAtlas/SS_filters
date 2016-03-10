a1a91e690e02de1d56bc337d41cb5342aee56c6c
MattAtlas <saltattam@gmail.com>
2016-03-10 03:10:29 -0800

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -7,13 +7,33 @@ typedef struct SqrMatrix{
 	float** mat;
 }SqrMatrix;
 
+typedef struct Matrix{
+	int rows;
+	int cols;
+	float** mat;
+}Matrix;
+
+typedef struct SS_filter{
+	float dt;
+	int states;
+	int inputs;
+	int outputs;
+	SqrMatrix F;
+	Matrix G;
+	Matrix H;
+	float* X0;
+	float* X1;
+	float* Y;
+}SS_filter;
 
 SqrMatrix CreateSqrMatrix(int n);
-float * CreateVector(int n);
-int factorial(int num);
+Matrix CreateMatrix(int rows, int cols);
+float* CreateVector(int n);
+float* marchFilter(SS_filter* sys, float input[]);
+//int factorial(int num);
 
 SqrMatrix discrete_F(SqrMatrix A, float h);
 
-float * discrete_G(SqrMatrix A, float B[], float h);
-
+Matrix discrete_G(SqrMatrix A, Matrix B, float h);
+SS_filter CreateSSfilter(SqrMatrix A, Matrix B, Matrix C,float dt);
 #endif
