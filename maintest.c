d502ff87dcb703c6850b3e6fec7f051e34906e90
MattAtlas <saltattam@gmail.com>
2016-03-10 18:39:20 -0800

    Update maintest.c

--- maintest.c
+++ maintest.c
@@ -5,56 +5,39 @@
 
 int main()
 {
-	int i,j;
-	float h = 0.01;
-	float in = 1;
-	float* u;
-	u = &in;
-	//float* y;
-	SqrMatrix A = CreateSqrMatrix(3);
-	Matrix B = CreateMatrix(3,1);
-	Matrix C = CreateMatrix(1,3);
-	C.mat[0][1] = 1;
-	C.mat[0][2] = 5;
-	
-	B.mat[0][0] = 1;
-	
+	float h = 0.1;
+
+	Matrix A = CreateSqrMatrix(3);
+	Matrix B = CreateColumnVector(3);
+	Matrix C = CreateRowVector(3);
+	Matrix u = CreateColumnVector(1);
+
 	A.mat[0][0] = -5;
 	A.mat[0][1] = -5;
 	A.mat[0][2] = -5;
-	
 	A.mat[1][0] = 1;
 	A.mat[2][1] = 1;
 	
-	SS_filter sys;
-	sys = CreateSSfilter(A,B,C,h);
+	B.mat[0][0] = 1;
+
+	C.mat[0][1] = 1;
+	C.mat[0][2] = 5;	
 	
+	u.mat[0][0] = 1;
 	
-	sys.X0[0] = 0;
-	sys.X0[1] = 0;
-	sys.X0[2] = 0;
+	SS_filter sys = CreateSSfilter(A,B,C,h);
 	
-	float* y;
+	sys.X0.mat[0][0] = 0;
+	sys.X0.mat[1][0] = 0;
+	sys.X0.mat[2][0] = 0;	
 	
-	for (int i=0;i<100;i++){
-		y = marchFilter(sys,u);
+	for (int i=0;i<250;i++){
+		marchFilter(&sys,u);
 	}
-	printf("\n");
-	for (int i=0;i<sys.states;i++){
-		for (int j=0;j<sys.states;j++){
-			printf("%0.10f\t",sys.F.mat[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	for (int i=0;i<sys.states;i++){
-		for (int j=0;j<sys.inputs;j++){
-			printf("%0.10f\t",sys.G.mat[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	printf("y = %f\n",*y);
-	printf("states = %d\ninputs = %d\noutputs = %d\n",sys.states,sys.inputs,sys.outputs);
+	
+	printMatrix(sys.F);
+	
+	printMatrix(sys.G);
+	
 	return 0;
 }
