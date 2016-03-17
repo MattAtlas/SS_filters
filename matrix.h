cab699a03e71f3aa5ad5d844d987daf54ca4f313
MattAtlas <saltattam@gmail.com>
2016-03-17 14:39:26 -0700

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -13,12 +13,16 @@ typedef struct SS_filter{
 	int states;
 	int inputs;
 	int outputs;
+	int saturation_en;
+	Matrix saturation_high;
+	Matrix saturation_low;
 	Matrix F;
 	Matrix G;
-	Matrix H;
+	Matrix H; 
 	Matrix X0;
 	Matrix X1;
 	Matrix Y;
+	Matrix K;
 }SS_filter;
 
 // all arrays, 1D & 2D are stored in a matrix struct
@@ -30,15 +34,15 @@ int getVectorEntry(Matrix* A, int pos, float* val);
 Matrix CreateSqrMatrix(int n);
 Matrix CreateRowVector(int n);
 Matrix CreateColumnVector(int n);
-
+Matrix duplicateMatrix(Matrix A);
 // Matrix operations
 int multiplyMatrices(Matrix* A, Matrix* B, Matrix* out);
 int addMatrices(Matrix* A, Matrix* B, Matrix* out);
 int invertMatrix(Matrix* A, Matrix* out);
+int scalarMultiply(Matrix A, float s);
+float getDetMatrix(Matrix A);
 
-float getDetMatrix(Matrix* A);
-
-void printMatrix(Matrix* A);
+void printMatrix(Matrix A);
 
 // convertions from continuous to discrete time
 Matrix C2D_A2F(Matrix A, float h);
@@ -48,7 +52,7 @@ Matrix C2D_B2G(Matrix A, Matrix B, float h);
 SS_filter CreateSSfilter(Matrix A, Matrix B, Matrix C,float dt);
 
 // March discrete filter forward using a new input
-// returns 1 or 0 for success or failure
+// returns 0 or -1 for success or failure
 // user can grab new output from sys
 int marchFilter(SS_filter* sys, Matrix input);
 
