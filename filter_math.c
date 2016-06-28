440efb69643f2c2ead22d86c2c4f84afdeff942d
MattAtlas <saltattam@gmail.com>
2016-06-28 16:58:00 -0700

    Update filter_math.c

--- filter_math.c
+++ filter_math.c
@@ -10,10 +10,10 @@
 #include <stdlib.h>
 
 #define DEBUG
-
+#define PI 3.14159265
 
 /*******************************************************************************
-* 
+* CT_SS_filter_t createCTSSfilter(int states, int inputs, int outputs)
 *
 * Sets up all matrices for system
 *******************************************************************************/
@@ -47,7 +47,7 @@ CT_SS_filter_t createCTSSfilter(int states, int inputs, int outputs){
 
 
 /*******************************************************************************
-* 
+* DT_SS_filter_t createDTSSfilter(CT_SS_filter_t* CT_sys, float dt)
 *
 * Sets up all matrices for system
 *******************************************************************************/
@@ -65,7 +65,7 @@ DT_SS_filter_t createDTSSfilter(CT_SS_filter_t* CT_sys, float dt){
 	filter.Xnew = createVector(filter.states);
 	filter.Y    = createVector(filter.outputs);
 	
-	filter.F = createSqrareMatrix(filter.states);
+	filter.F = createSquareMatrix(filter.states);
 	filter.G = createMatrix(filter.states,filter.inputs);
 	filter.H = createMatrix(filter.outputs,filter.states);
 
@@ -88,7 +88,7 @@ DT_SS_filter_t createDTSSfilter(CT_SS_filter_t* CT_sys, float dt){
 
 
 /*******************************************************************************
-* 
+* int tf2ss(vector_t b, vector_t a, CT_SS_filter_t* CT_sys)
 *
 * 
 *******************************************************************************/
@@ -99,16 +99,13 @@ int tf2ss(vector_t b, vector_t a, CT_SS_filter_t* CT_sys){
 		printf("Error: Improper transfer function\n");
 		return -1;
 	}
-	
 	// make new b vector the same length as a
 	vector_t b_temp = createVector(a.len);
 	// allocate memory for zero coeffs
-
 	// put the values of b in new b starting at the end	
 	for(i=1;i<=b.len;i++){
 		b_temp.data[a.len - i] = b.data[b.len - i];
 	}
-
 	if(a.data[0] != 1){
 		float coeff = a.data[0];
 		for(i=0;i<=a.len;i++){	
@@ -116,28 +113,21 @@ int tf2ss(vector_t b, vector_t a, CT_SS_filter_t* CT_sys){
 			b_temp.data[i] = b_temp.data[i] / coeff;
 		}
 	}
-
 	// Fill in A matrix
 	for(i=0;i<a.len-2;i++){
 		CT_sys->A.data[i+1][i] = 1;			// fill in lower identity
 	}
 	for(i=0;i<a.len-1;i++){
-		CT_sys->A.data[0][i] = -a.data[0][i+1];	// fill in top row
+		CT_sys->A.data[0][i] = -a.data[i+1];	// fill in top row
 	}
-	printMatrix(CT_sys->A);
-	
+
 	CT_sys->B.data[0][0] = 1;
-	
+
 	for(i=0;i<a.len-1;i++){
 		CT_sys->C.data[0][i] = b_temp.data[i+1];
 	}
-	
 	return 0;
 }
-
-
-
-
 
 /*******************************************************************************
 * 
@@ -166,14 +156,14 @@ int tf2ss(vector_t b, vector_t a, CT_SS_filter_t* CT_sys){
 // }
 
 /*******************************************************************************
-* 
+* int marchFilter(DT_SS_filter_t* DT_sys, vector_t input)
 *
 * 
 *******************************************************************************/
 int marchFilter(DT_SS_filter_t* DT_sys, vector_t input){
 	
-	matrix_t FX;	// temporary matrix structs
-	matrix_t Gu;
+	vector_t FX;	// temporary vectors
+	vector_t Gu;
 	int i;
 	
 	DT_sys->Xold = DT_sys->Xnew;
@@ -196,14 +186,15 @@ int marchFilter(DT_SS_filter_t* DT_sys, vector_t input){
 		}
 	}
 	
-	FX = matrixMultiply(DT_sys->F, DT_sys->Xold);
-	Gu = matrixMultiply(DT_sys->G, input);
+	FX = matrixTimesColVec(DT_sys->F, DT_sys->Xold);
+	Gu = matrixTimesColVec(DT_sys->G, input);
 	
-	DT_sys->Xnew = matrixAdd(FX, Gu);
-
+	for(i=0;i<input.len;i++){
+		DT_sys->Xnew.data[i] = FX.data[i] + Gu.data[i];
+	}
 	#ifdef DEBUG
 	// output y is estimate of sensor data
-	printf("y = %f\n",DT_sys->Xold.data[2][0]*DT_sys->H.data[0][2]);
+	//printf("y = %f\n",DT_sys->Xold.data[2][0]*DT_sys->H.data[0][2]);
 	#endif
 	
 	return 0;
@@ -219,9 +210,9 @@ int marchFilter(DT_SS_filter_t* DT_sys, vector_t input){
 matrix_t C2D_A2F(matrix_t A, float h){
 	int m = A.rows;
 	int i,j,k,N;
-	matrix_t sumold = createSqrareMatrix(m);
-	matrix_t sumnew = createSqrareMatrix(m);
-	matrix_t result = createSqrareMatrix(m);
+	matrix_t sumold = createSquareMatrix(m);
+	matrix_t sumnew = createSquareMatrix(m);
+	matrix_t result = createSquareMatrix(m);
 	float sum;
 
 	// initialize identity matrix
@@ -255,18 +246,18 @@ matrix_t C2D_A2F(matrix_t A, float h){
 
 	
 /*******************************************************************************
-* 
+* matrix_t C2D_B2G(matrix_t A, matrix_t B, float h)
 *
 * Using CT A and B matrices and time step, get DT G matrix
 *******************************************************************************/
 matrix_t C2D_B2G(matrix_t A, matrix_t B, float h){
 	int i,j,k,N;
 	int m = A.rows;
-	matrix_t sumold = CreateSqrMatrix(m);
-	matrix_t sumnew = CreateSqrMatrix(m);
-	matrix_t result = CreateSqrMatrix(m);
+	matrix_t sumold = createSquareMatrix(m);
+	matrix_t sumnew = createSquareMatrix(m);
+	matrix_t result = createSquareMatrix(m);
 	float sum;
-	matrix_t G = CreateMatrix(B.rows,B.cols);
+	matrix_t G = createMatrix(B.rows,B.cols);
 
 	// initialize identity matrix
 	for(i=0;i<m;i++){		
@@ -307,11 +298,14 @@ matrix_t C2D_B2G(matrix_t A, matrix_t B, float h){
 }
 
 
-
-vector_t convolve(vector_t v1, vector_t v2){
+/*******************************************************************************
+* int polyConv(vector_t v1, vector_t v2, vector_t* out)
+*
+* 
+*******************************************************************************/
+int polyConv(vector_t v1, vector_t v2, vector_t* out){
 	
 	int m,n,i,j,k;
-	vector_t out;
 	if(!v1.initialized || !v2.initialized){
 		printf("ERROR: vector not initialized yet\n");
 		return -1;
@@ -319,12 +313,105 @@ vector_t convolve(vector_t v1, vector_t v2){
 	m = v1.len;
 	n = v2.len;
 	k = m+n-1;
-	out = createVector(k);
-	
+	vector_t C = createVector(k);
 	for(i=0;i<m;i++){
-		for(j=0;j<n;i++){
-			out(i+j) += u(i) * v(j);
+		for(j=0;j<n;j++){
+			C.data[i+j] += v1.data[i] * v2.data[j];
 		}
 	}
-	return out;	
+	*out = C;
+	return 0;	
 }
+
+/*******************************************************************************
+* int polyPow(vector_t* v, int N)
+*
+* 
+*******************************************************************************/
+int polyPow(vector_t* v, int N){
+	
+	int i;
+	if(!v->initialized){
+		printf("ERROR: vector not initialized yet\n");
+		return -1;
+	}
+	if(N < 2){
+		printf("ERROR: order must be > 1\n");
+		return -1;
+	}
+	vector_t temp;
+	for(i=2;i<=N;i++){
+		polyConv(*v, *v, &temp);
+		*v = temp;
+		printf("v = conv(v,v)\n");
+		printVectorSciNotation(*v);
+	}
+	return 0;
+}
+
+/*******************************************************************************
+* vector_t butterPoly(int N, float wc)
+*
+* Return vector of continuous Butterworth denominator or order N and cutoff wc
+*******************************************************************************/
+vector_t butterPoly(int N, float wc){
+	int i;
+	vector_t filter, P2, P3, temp;
+	if(N < 1){
+		printf("ERROR: order must be > 1\n");
+		return filter;
+	}
+	filter = createVector(1);
+	filter.data[0] = 1;
+	P2 = createVector(2);
+	P3 = createVector(3);
+	if(N%2 == 0){
+		for(i=1;i<=N/2;i++){
+			P3.data[0] = 1/(wc*wc);
+			P3.data[1] = -2*cos((2*i + N - 1)*PI/(2*N))/wc;
+			P3.data[2] = 1;
+			temp = duplicateVector(filter);
+			polyConv(temp,P3,&filter);
+			destroyVector(&temp);
+		}
+	}
+	if(N%2 == 1){	
+		P2.data[0] = 1/wc;
+		P2.data[1] = 1;
+		temp = duplicateVector(filter);
+		polyConv(temp,P2,&filter);
+		destroyVector(&temp);
+		for(i=1;i<=(N-1)/2;i++){
+			P3.data[0] = 1/(wc*wc);
+			P3.data[1] = -2*cos((2*i + N - 1)*PI/(2*N))/wc;
+			P3.data[2] = 1;
+			temp = duplicateVector(filter);
+			polyConv(temp,P3,&filter);
+			destroyVector(&temp);
+		}
+	}
+	destroyVector(&P2);
+	destroyVector(&P3);
+	return filter;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
